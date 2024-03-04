12f0b111da8f3a2fc984c9ed9cf149d7be04d58f
pbone <47093312+Pbone3@users.noreply.github.com>
2024-03-03 22:59:43 -0600

    Refactored drawing code to allow for adding new loadouts

--- GUI/ExLoadoutButtons.cs
+++ GUI/ExLoadoutButtons.cs
@@ -9,16 +9,20 @@ using Terraria.ModLoader;
 namespace ExtraLoadouts.GUI {
     public class ExLoadoutButtons : ILoadable {
         public enum LoadoutStatus {
-            Enabled,
-            DisabledButOccupied,
-            Disabled
+            Display,
+            DisplayWithWarning,
+            Hide
         }
 
+        public const int LoadoutButtons_HoriFrames = 4;
+        public const int LoadoutButtons_VertiFrames = ExtraLoadoutsMod.EXTRA_LOADOUTS;
         public static Asset<Texture2D> LoadoutButtonsTexture = null;
 
         void ILoadable.Load(Mod mod) {
             if (!Main.dedServ) {
-                Main.QueueMainThreadAction(() => LoadoutButtonsTexture = mod.Assets.Request<Texture2D>("Assets/ExLoadouts"));
+                Main.QueueMainThreadAction(
+                    () => LoadoutButtonsTexture = mod.Assets.Request<Texture2D>("GUI/ExLoadoutButtons")
+                );
             }
         }
 
@@ -40,12 +44,27 @@ namespace ExtraLoadouts.GUI {
             int buttonHeight = 32;
             int buttonSpacing = 4;
 
-            LoadoutStatus[] statuses = GetLoadoutStatuses();
-            int loadoutsToDraw = statuses.Count(LoadoutStatus.Enabled) + statuses.Count(LoadoutStatus.DisabledButOccupied);
-            Rectangle[] buttonHitboxes = new Rectangle[loadoutsToDraw];
+            LoadoutStatus[] loadoutStatus = GetLoadoutStatuses();
+            Rectangle[] buttonHitboxes = new Rectangle[ExtraLoadoutsMod.EXTRA_LOADOUTS];
 
-            for (int i = 0; i < loadoutsToDraw; i++) {
-                buttonHitboxes[i] = new(rectangle.X + rectangle.Width, rectangle.Y + (buttonHeight + buttonSpacing) * (ExtraLoadoutsMod.VANILLA_LOADOUTS + i), 32, 32);
+            Rectangle warningFrame = LoadoutButtonsTexture.Frame(
+                LoadoutButtons_HoriFrames, LoadoutButtons_VertiFrames,
+                3, 0
+            );
+
+            for (int i = 0; i < ExtraLoadoutsMod.EXTRA_LOADOUTS; i++) {
+                if (loadoutStatus[i] == LoadoutStatus.Hide) {
+                    continue;
+                }
+
+                GetLoadoutFrames(i, out var inactiveFrame, out var activeFrame, out var outlineFrame);
+                buttonHitboxes[i] =
+                    new(
+                        rectangle.X + rectangle.Width,
+                        rectangle.Y + (buttonHeight + buttonSpacing) * (ExtraLoadoutsMod.VANILLA_LOADOUTS + i),
+                        32,
+                        32
+                    );
 
                 bool hovered = false;
                 if (buttonHitboxes[i].Contains(Main.MouseScreen.ToPoint())) {
@@ -54,7 +73,7 @@ namespace ExtraLoadouts.GUI {
                     
                     if (!Main.mouseText) {
                         string text = Language.GetTextValue("Mods.ExtraLoadouts.GUI.Loadout" + (ExtraLoadoutsMod.VANILLA_LOADOUTS + 1 + i));
-                        if (statuses[i] == LoadoutStatus.DisabledButOccupied) {
+                        if (loadoutStatus[i] == LoadoutStatus.DisplayWithWarning) {
                             text += "\n";
                             text += Language.GetTextValue("Mods.ExtraLoadouts.GUI.DisabledButOccupied");
                         }
@@ -67,25 +86,52 @@ namespace ExtraLoadouts.GUI {
                     }
                 }
 
-                int frameX = i == modPlayer.CurrentExLoadoutIndex ? 1 : 0;
-                Rectangle frame = LoadoutButtonsTexture.Frame(3, 4, frameX, i);
-
                 Vector2 pos = buttonHitboxes[i].Center.ToVector2();
-                Vector2 origin = frame.Size() / 2f;
+                // All frames are the same size
+                Vector2 origin = activeFrame.Size() / 2f;
 
                 // Draw main button
-                Main.spriteBatch.Draw(LoadoutButtonsTexture.Value, pos, frame, Color.White, 0f, origin, 1f, SpriteEffects.None, 0f);
+                Main.spriteBatch.Draw(
+                    LoadoutButtonsTexture.Value,
+                    pos,
+                    modPlayer.CurrentExLoadoutIndex == i ? activeFrame : inactiveFrame,
+                    Color.White,
+                    0f,
+                    origin,
+                    1f,
+                    SpriteEffects.None,
+                    0f
+                );
 
                 // Draw outline if hovered
                 if (hovered) {
-                    frame = LoadoutButtonsTexture.Frame(3, 4, 2, i);
-                    Main.spriteBatch.Draw(LoadoutButtonsTexture.Value, pos, frame, Main.OurFavoriteColor, 0f, origin, 1f, SpriteEffects.None, 0f);
+                    // Draw main button
+                    Main.spriteBatch.Draw(
+                        LoadoutButtonsTexture.Value,
+                        pos,
+                        outlineFrame,
+                        Main.OurFavoriteColor,
+                        0f,
+                        origin,
+                        1f,
+                        SpriteEffects.None,
+                        0f
+                    );
                 }
 
                 // If the loadout is disabled in the config but still has items, draw an ! over it
-                if (statuses[i] == LoadoutStatus.DisabledButOccupied) {
-                    frame = LoadoutButtonsTexture.Frame(3, 4, 0, 3);
-                    Main.spriteBatch.Draw(LoadoutButtonsTexture.Value, pos, frame, Color.White, 0f, origin, 1f, SpriteEffects.None, 0f);
+                if (loadoutStatus[i] == LoadoutStatus.DisplayWithWarning) {
+                    Main.spriteBatch.Draw(
+                        LoadoutButtonsTexture.Value,
+                        pos,
+                        warningFrame,
+                        Color.White,
+                        0f,
+                        origin,
+                        1f,
+                        SpriteEffects.None,
+                        0f
+                    );
                 }
             }
         }
@@ -101,17 +147,35 @@ namespace ExtraLoadouts.GUI {
 
                 if (i < ModContent.GetInstance<LoadoutsConfig>().ExtraLoadouts) {
                     // It's enabled; all good!
-                    statuses[i] = LoadoutStatus.Enabled;
-                    continue;
+                    statuses[i] = LoadoutStatus.Display;
+                } else if (modPlayer.CurrentExLoadoutIndex == i) {
+                    bool occupied = player.armor.Any(item => !item.IsLikelyNone()) || player.dye.Any(item => !item.IsLikelyNone());
+                    statuses[i] = occupied ? LoadoutStatus.DisplayWithWarning : LoadoutStatus.Display;
                 } else {
                     // If it contains items, keep it visible (with a warning) so people can empty it
                     bool occupied = loadout.Armor.Any(item => !item.IsLikelyNone()) || loadout.Dye.Any(item => !item.IsLikelyNone());
-                    statuses[i] = occupied ? LoadoutStatus.DisabledButOccupied : LoadoutStatus.Disabled;
-                    continue;
+                    statuses[i] = occupied ? LoadoutStatus.DisplayWithWarning : LoadoutStatus.Hide;
                 }
             }
 
             return statuses;
+        }
+
+        public static void GetLoadoutFrames(int loadout, out Rectangle inactive, out Rectangle active, out Rectangle outline) {
+            inactive = LoadoutButtonsTexture.Frame(
+                LoadoutButtons_HoriFrames, LoadoutButtons_VertiFrames,
+                0, loadout
+            );
+
+            active = LoadoutButtonsTexture.Frame(
+                LoadoutButtons_HoriFrames, LoadoutButtons_VertiFrames,
+                1, loadout
+            );
+
+            outline = LoadoutButtonsTexture.Frame(
+                LoadoutButtons_HoriFrames, LoadoutButtons_VertiFrames,
+                2, loadout
+            );
         }
 
         void ILoadable.Unload() {
